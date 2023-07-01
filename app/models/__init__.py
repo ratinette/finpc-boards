fb84f2a1836d60d2417d9e7159085bce18f5235b
hanyul99 <ehanyul99@gmail.com>
2023-07-01 17:44:54 +0900

    Board Modeling

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,2 @@
 from app.models.dog_models import *
+from app.models.board_models import *
--- app/models/board_models.py
+++ app/models/board_models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -12,7 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="posts")
-    author = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -21,6 +22,6 @@ class Comment(models.Model):
     content = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     parent = models.ForeignKey("self", on_delete=models.CASCADE, related_name="replies", null=True)
-    author = models.ForeignKey("User", on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
